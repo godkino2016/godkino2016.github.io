28774c03645e63d032cc84a53120de79dc5c5cd6
det-psiholog <shmat.evg@yandex.ru>
2016-12-25 19:10:54 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $("#owl-one").owlCarousel({
+      items: 1,
+      loop: true,
+      center: true,
+      autoplay: true,
+      mouseDrag: false,
+      touchDrag: false,
+      autoplayTimeout: 15000,
+  })
+   
+});
+
+$(document).ready(function(){
+  $("#owl-two").owlCarousel({
+      items: 1,
+      loop: true,
+      center: true,
+      autoplay: true,
+      mouseDrag: false,
+      touchDrag: false,
+      autoplayTimeout: 15000,
+  })
+   
+});
